6d5758668d62fdca6b478cadae5acefd8dc355a3
parikshitco <parikshit.shaktawat@goquant.io>
2025-02-16 19:36:02 +0000

    testing by hiding aws 2

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
     "Set-ExecutionPolicy Bypass -Scope Process -Force; ^
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; ^
-    iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/parikshitco/gq-cloud-cli/main/uninstall.ps1'))"
+    iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/parikshitco/gq-cloud-cli/main/install.ps1'))"
 pause
