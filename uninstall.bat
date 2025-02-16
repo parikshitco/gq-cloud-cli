c2519e7cd63ef9f0bf889b21b4e05edb82b3cf20
parikshitco <parikshit.shaktawat@goquant.io>
2025-02-16 20:05:40 +0000

    working windows install and uninstall script

--- uninstall.bat
+++ uninstall.bat
@@ -1,6 +1,3 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-    "Set-ExecutionPolicy Bypass -Scope Process -Force; ^
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; ^
-    iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/parikshitco/gq-cloud-cli/main/uninstall.ps1'))"
-pause
+:: Open PowerShell as Administrator and keep window open
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& { Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(''https://raw.githubusercontent.com/parikshitco/gq-cloud-cli/main/uninstall.ps1'')); pause""' -Verb RunAs }"
